993bd4ecdb6f9e55348fa59713346241d2ec20c7
pedro.nadu.rangel <pedro.nadu.rangel@accenture.com>
2022-12-11 15:42:07 -0300

    chore: Criar Gui

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import gui
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def search(location):
+    print("Search 1: " + location)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def search_all():
+    print("Search 2")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    gui.MainGui().create_screen()
